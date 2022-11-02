20b853fc7cbb3fb56e340341d2dba449fa968b46
Serhii Yatsenko <serhii2111@yahoo.com>
2022-11-02 02:56:57 +0200

    Update one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+     <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}" 
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join('');
+}
+
+function onGalleryContainerClick(evt) {         
+    evt.preventDefault();
+
+    const isGalleryItem = evt.target.classList.contains('gallery__image');
+
+    if (!isGalleryItem) {
+        return;
+    }
+
+    const instance = basicLightbox.create
+        (`img
+            src = ${evt.target.dataset.source}
+            alt="${evt.target.alt}"/>`)
+
+    // console.log(evt.target);
+    instance.show()
+}
+
+// console.log(galleryItems);
