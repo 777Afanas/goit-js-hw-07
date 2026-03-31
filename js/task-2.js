1f92425e77a3017803aa397c9248bd71d61deae2
777Afanas <serhii2111@yahoo.com>
2026-03-31 23:44:32 +0300

    add task four

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,14 @@
+// Напиши скрипт для створення галереї зображень на
+// основі масиву даних. HTML містить список ul.gallery.
+// Mожеш створити й додати HTML-елементи, використовуючи
+// document.createElement() і elem.append() або
+// шаблонні рядки і elem.insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися
+// в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї
+// флексбоксами через CSS класи.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,18 +23,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
   {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Elephant Beside on Baby Elephant',
   },
   {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Blue Geeen and Orange Parrot',
   },
   {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Zebras on Zebra',
   },
 ];
